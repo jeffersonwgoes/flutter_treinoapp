55499e81019b1fc5ae2559f395507ac71f597a79
JeffersonGoes <jeffersonwgoes@gmail.com>
2020-01-14 22:54:46 -0300

    Funcionamento de fluxo básico está funcionando

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,11 @@ class MyApp extends StatelessWidget {
 
 class ListWorkouts extends StatefulWidget {
   @override
+  List<Workout> workouts = List();
   _ListWorkoutsState createState() => _ListWorkoutsState();
 }
 
 class _ListWorkoutsState extends State<ListWorkouts> {
-  List<Workout> workouts = List();
   @override
   Widget build(BuildContext context) {
     // workouts.add(Workout("Treino Sexta", "Seg-Terça-Quarta"));
@@ -32,16 +32,14 @@ class _ListWorkoutsState extends State<ListWorkouts> {
             Navigator.of(context)
                 .push(MaterialPageRoute(builder: (context) => FormAddWorkout()))
                 .then((workout) {
-              debugPrint("Workout: " +
-                  workout.name +
-                  " Days: " +
-                  workout.getDaysOfWeek());
+              this.widget.workouts.add(workout);
             });
           },
         ),
         body: ListView.builder(
-          itemCount: workouts.length,
-          itemBuilder: (context, index) => ItemListWorkouts(workouts[index]),
+          itemCount: this.widget.workouts.length,
+          itemBuilder: (context, index) =>
+              ItemListWorkouts(this.widget.workouts[index]),
         ));
   }
 }
@@ -55,7 +53,11 @@ class ItemListWorkouts extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        onTap: () => debugPrint('Tap no ' + this.workout.name),
+        onTap: () => {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+            return ListExercises(workout);
+          }))
+        },
         title: Text(this.workout.name),
         subtitle: Text(this.workout.getDaysOfWeek()),
       ),
@@ -66,25 +68,24 @@ class ItemListWorkouts extends StatelessWidget {
 class Workout {
   final String name;
   final List<bool> daysOfWeek;
+  List<Exercise> exercises = List();
 
   Workout(this.name, this.daysOfWeek);
 
   String getDaysOfWeek() {
     String _daysOfWeek = "";
     _daysOfWeek += this.daysOfWeek[0] == true ? 'Seg' : '';
-    _daysOfWeek += (_daysOfWeek.length > 0 ? ' - ' : '') +
-        (this.daysOfWeek[1] == true ? 'Ter' : '');
-    _daysOfWeek += (_daysOfWeek.length > 0 ? ' - ' : '') +
-        (this.daysOfWeek[2] == true ? 'Qua' : '');
-    _daysOfWeek += (_daysOfWeek.length > 0 ? ' - ' : '') +
-        (this.daysOfWeek[3] == true ? 'Qui' : '');
-    _daysOfWeek += (_daysOfWeek.length > 0 ? ' - ' : '') +
-        (this.daysOfWeek[4] == true ? 'Sex' : '');
-    _daysOfWeek += (_daysOfWeek.length > 0 ? ' - ' : '') +
-        (this.daysOfWeek[5] == true ? 'Sab' : '');
-    _daysOfWeek += (_daysOfWeek.length > 0 ? ' - ' : '') +
-        (this.daysOfWeek[6] == true ? 'D,on' : '');
+    _daysOfWeek += (this.daysOfWeek[1] == true ? 'Ter' : '');
+    _daysOfWeek += (this.daysOfWeek[2] == true ? 'Qua' : '');
+    _daysOfWeek += (this.daysOfWeek[3] == true ? 'Qui' : '');
+    _daysOfWeek += (this.daysOfWeek[4] == true ? 'Sex' : '');
+    _daysOfWeek += (this.daysOfWeek[5] == true ? 'Sab' : '');
+    _daysOfWeek += (this.daysOfWeek[6] == true ? 'Dom' : '');
     return _daysOfWeek;
+  }
+
+  void addExercise(exercise) {
+    this.exercises.add(exercise);
   }
 }
 
@@ -165,9 +166,6 @@ class _FormAddWorkoutState extends State<FormAddWorkout> {
                 style: TextStyle(fontSize: 16.0),
               ),
               onPressed: () {
-                for (bool item in _chbDay) {
-                  debugPrint('item: ' + item.toString());
-                }
                 Navigator.pop(context, Workout(_nameController.text, _chbDay));
               },
             ),
@@ -202,6 +200,127 @@ class _CheckBoxDayOfWeekState extends State<CheckBoxDayOfWeek> {
           style: TextStyle(fontSize: 16.0),
         ),
       ],
+    );
+  }
+}
+
+class ListExercises extends StatefulWidget {
+  Workout _workout;
+
+  ListExercises(this._workout);
+
+  @override
+  _ListExercisesState createState() => _ListExercisesState();
+}
+
+class _ListExercisesState extends State<ListExercises> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Exercises'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            Navigator.of(context)
+                .push(
+                    MaterialPageRoute(builder: (context) => FormAddExercise()))
+                .then((_exercise) {
+              this.widget._workout.addExercise(_exercise);
+            });
+          },
+        ),
+        body: ListView.builder(
+          itemCount: this.widget._workout.exercises.length,
+          itemBuilder: (context, index) =>
+              ItemListExercise(this.widget._workout.exercises[index]),
+        ));
+  }
+}
+
+class Exercise {
+  final String name;
+  final int weight;
+  final int time;
+  final int repetitions;
+
+  Exercise(this.name, {this.weight, this.time, this.repetitions});
+
+  @override
+  String toString() {
+    return "Its toString bitch";
+  }
+}
+
+class FormAddExercise extends StatefulWidget {
+  @override
+  _FormAddExerciseState createState() => _FormAddExerciseState();
+}
+
+class _FormAddExerciseState extends State<FormAddExercise> {
+  TextEditingController _nameController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Workout"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              style: TextStyle(fontSize: 16.0),
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: 'Nome',
+                hintText: 'Nome do Exercicio',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              style: TextStyle(fontSize: 16.0),
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: 'Nome',
+                hintText: 'Nadas',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: RaisedButton(
+              child: Text(
+                "Add Workout",
+                style: TextStyle(fontSize: 16.0),
+              ),
+              onPressed: () {
+                Navigator.pop(context, Exercise(_nameController.text));
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ItemListExercise extends StatelessWidget {
+  final Exercise exercise;
+
+  ItemListExercise(this.exercise);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: () => {},
+        title: Text(this.exercise.name),
+        subtitle: Text(this.toString()),
+      ),
     );
   }
 }
